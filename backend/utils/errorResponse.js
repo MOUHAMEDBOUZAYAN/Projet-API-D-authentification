dc6f28146ea15bbbfa619c3cce2019e61b88b427
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2025-05-08 14:45:34 +0100

    Ajout de la classe ErrorResponse pour la gestion des réponses d'erreur personnalisées

---
+++ backend/utils/errorResponse.js
@@ -0,0 +1,11 @@
+// utils/errorResponse.js
+// Classe personnalisée pour les réponses d'erreur
+
+class ErrorResponse extends Error {
+    constructor(message, statusCode) {
+      super(message);
+      this.statusCode = statusCode;
+    }
+  }
+  
+  module.exports = ErrorResponse;
