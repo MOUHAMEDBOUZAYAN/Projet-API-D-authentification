b3d8a0c00841e130ca85bac6a8ca1ce16e76a3e0
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2025-05-10 21:25:53 +0100

    Mise à jour de la structure du projet avec des améliorations dans les fichiers HTML, CSS et de configuration. Ajout de la gestion des routes, intégration de la bibliothèque de notifications et personnalisation des styles avec Tailwind CSS.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          success: {
+            duration: 3000,
+            theme: {
+              primary: '#4aed88',
+            },
+          },
+          error: {
+            duration: 4000,
+            theme: {
+              primary: '#ff5252',
+            },
+          },
+        }}
+      />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
